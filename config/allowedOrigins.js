ea65579a3bf0135fb4c9476554d32820494ab849
Katrina Campbell <ktrncampbell@gmail.com>
2022-11-16 16:22:28 -0500

    initial commit

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.kcsrepairshop.com',
+    'https://kcsreparishop.com'
+]
+
+module.exports = allowedOrigins
---
+++ controllers/usersController.js
@@ -0,0 +1,101 @@
+const User = require('../models/User')
+const Note = require('../models/Note')
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+//@desc Get all users
+//@route GET /users
+const getaAllUsers = asyncHandler(async (req, res) => {
+    const users = await User.find().select('-password').lean()
+    if(!users) {
+        return res.status(400).json({ message: 'No users found' })
+    }
+    res.json(users)
+})
+
+//@desc Create new user
+//@route POST /users
+const addNewUser = asyncHandler(async (req, res) => {
+    const { username, password, roles } = req.body
+    if(!username || !password || !Array.isArray(roles) || !roles.length) {
+        return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    const duplicate = await User.findOne({ username }).lean().exec()
+    if(duplicate) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    const hashedPwd = await bcrypt.hash(password, 10) //salt rounds
+
+    const userObject = { username, 'password': hashedPwd, roles }
+
+    const user = await User.create(userObject)
+    if(user) {
+        res.status(201).json({ message: `New user ${username} created`})
+    } else {
+        return res.status(400).json({ message: 'Invalid user data received' })
+    }
+})
+
+//@desc Update a user
+//@route PATCH /users
+const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, roles, active, password } = req.body
+    if(!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+    return res.status(400).json({ message: 'All fields are required' })
+    }
+
+    const user = await User.findById(id).exec()
+    if(!user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
+
+    const duplicate = await User.findOne({ username }).lean().exec()
+    if(duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: 'Duplicate username' })
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+    if(password) {
+        user.password = await bcrypt.hash(password, 10)
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({ message: `${updatedUser.username} updated` })
+})
+
+//@desc Delete a user
+//@route DELETE /users
+const deleteUser = asyncHandler(async (req, res) => {
+    const { id } = req.body
+    if(!id) {
+        return res.status(400).json({ message: 'User ID required' })
+    }
+
+    const notes = await Note.findOne({ user: id }).lean().exec()
+    if(notes?.length) {
+        return res.status(400).json({ message: 'User has assigned notes' })
+    }
+
+    const user = await User.findById(id).exec()
+    if(!user) {
+        return res.status(400).json({ message: 'User not found' })
+    }
+
+    const result = await user.deleteOne()
+    const reply = `Username ${result.username} with ID ${result.id} deleted`
+    res.json(reply)
+})
+
+
+module.export = {
+    getaAllUsers,
+    addNewUser,
+    updateUser,
+    deleteUser
+}
+
